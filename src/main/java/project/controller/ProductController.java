b659c95dc2e47b0510588e70f59309b4a0492a9f
nguyenminhthoa1002 <minhthoa100299@gmail.com>
2023-03-19 02:24:01 +0700

    feature/#828/search_and_sort_newest_product_by_name

--- src/main/java/project/controller/ProductController.java
+++ src/main/java/project/controller/ProductController.java
@@ -1,6 +1,7 @@
 package project.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,11 +12,13 @@ import project.model.entity.Review;
 import project.model.shopMess.Message;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import project.model.utility.Utility;
 
 
 @RestController
@@ -55,6 +58,17 @@ public class ProductController {
             List<ProductResponse> responses = productService.getTopRatedProduct(productFeatureRequest.getStartDate(), productFeatureRequest.getEndDate(), productFeatureRequest.getSize());
             return new ResponseEntity<>(responses, HttpStatus.OK);
         } catch (Exception ex) {
+            return ResponseEntity.badRequest().body(Message.ERROR_400);
+        }
+    }
+
+    @GetMapping("search_sort_newest_product")
+    public ResponseEntity<?> searchAndSortNewestProductByName(@RequestParam Map<String,String> headers){
+        try {
+            Pageable pageable = Utility.sort_order(headers);
+            Map<String,Object> result = productService.findByName(headers.get("searchName"),pageable);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (Exception e){
             return ResponseEntity.badRequest().body(Message.ERROR_400);
         }
     }
