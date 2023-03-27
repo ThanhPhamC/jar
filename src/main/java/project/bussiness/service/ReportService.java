317a656de673b23f35a984293f252f53e38a0e71
ThanhPhamC <thanhphamhumg@gmail.com>
2023-03-27 09:12:03 +0700

    feature/#1102/file_excel_manager_task

--- src/main/java/project/bussiness/service/ReportService.java
+++ src/main/java/project/bussiness/service/ReportService.java
@@ -4,12 +4,13 @@ import org.springframework.http.ResponseEntity;
 import project.model.dto.response.ProductReportByBrand;
 import project.model.dto.response.ProductReportByCatalog;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
 public interface ReportService {
-    ResponseEntity<?> reportByAddress(Map<String,String> header );
+    ResponseEntity<?> reportByAddress(Map<String, String> header, HttpServletResponse response );
     List<ProductReportByCatalog> reportByCatalog(int status, int catId, LocalDateTime creDate, LocalDateTime endTime);
     List<ProductReportByBrand> reportByBrand(int status , int bradId, LocalDateTime createDate, LocalDateTime endDate);
 
--- src/main/java/project/controller/ReportController.java
+++ src/main/java/project/controller/ReportController.java
@@ -10,6 +10,7 @@ import project.model.dto.response.ProductReportByBrand;
 import project.model.dto.response.ProductReportByCatalog;
 import project.model.shopMess.Message;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
@@ -22,8 +23,8 @@ public class ReportController {
     private ReportService reportService;
     private CartService cartService;
         @GetMapping("/report_by_address")
-    public ResponseEntity<?> reportByAddress(@RequestParam Map<String,String> header ){
-        return reportService.reportByAddress(header);
+    public ResponseEntity<?> reportByAddress(@RequestParam Map<String, String> header, HttpServletResponse response ){
+        return reportService.reportByAddress(header,response);
     }
     @GetMapping("/proDuctByCatalog")
     public ResponseEntity<?>finProducByCatalog(@RequestParam int id,@RequestParam String startDate,@RequestParam String endDate){
